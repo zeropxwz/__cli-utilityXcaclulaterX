963103d30c5cf1dd7f4f3323ee2bf54156011684
zerop <zerop.development@gmail.com>
2022-08-06 01:59:13 +0300

    end

--- public/main.js
+++ public/main.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log('xwz');
+const readArguments = require('@zeropxwz/read-arguments');
+const args = readArguments(process.argv);
+function calc() {
+    var number = NaN;
+    var action = '';
+    var result = 0;
+    for (let i = 0; i < args.length; i++) {
+        if (i % 2 === 0) {
+            number = Number(args[i]);
+            switch (action) {
+                case '+':
+                    result += number;
+                    break;
+                case '-':
+                    result -= number;
+                    break;
+                case 'x':
+                    result *= number;
+                    break;
+                case '/':
+                    result /= number;
+                    break;
+                case '':
+                    result = number;
+                    break;
+                default:
+                    console.error('error: unknow operator');
+                    return;
+            }
+        }
+        if (i % 2 === 1) {
+            action = args[i];
+        }
+    }
+    console.log(result);
+}
+calc();
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,52 @@
-console.log('xwz')
+const readArguments = require('@zeropxwz/read-arguments')
+
+
+
+const args: string[] = readArguments(process.argv)
+
+
+
+function calc (): void {
+
+    var number: number = NaN
+    var action: string = ''
+
+    var result: number = 0
+
+    for (let i = 0; i < args.length; i++) {
+
+        if (i % 2 === 0) {
+            number = Number(args[i])
+
+            switch (action) {
+                case '+':
+                    result += number
+                    break
+                case '-':
+                    result -= number
+                    break
+                case 'x':
+                    result *= number
+                    break
+                case '/':
+                    result /= number
+                    break
+
+
+                case '':
+                    result = number
+                    break
+                default:
+                    console.error('error: unknow operator')
+                    return
+            }
+        }
+
+        if (i % 2 === 1) {
+            action = args[i]
+        }
+    }
+
+    console.log(result)
+}
+calc()
