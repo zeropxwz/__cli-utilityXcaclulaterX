f3cb72ce7bc11aa4404ffa7b928969707ca19f6f
zerop <zerop.development@gmail.com>
2022-08-06 03:00:08 +0300

    rewrite on class

--- public/main.js
+++ public/main.js
@@ -45,39 +45,3 @@ Calculator.operands = [];
 Calculator.operators = ['init'];
 Calculator.result = 0;
 Calculator.exec();
-// function calc (): void {
-//     const args: string[] = readArguments(process.argv)
-//     var number: number = NaN
-//     var action: string = ''
-//     var result: number = 0
-//     for (let i = 0; i < args.length; i++) {
-//         if (i % 2 === 0) {
-//             number = Number(args[i])
-//             switch (action) {
-//                 case '+':
-//                     result += number
-//                     break
-//                 case '-':
-//                     result -= number
-//                     break
-//                 case 'x':
-//                     result *= number
-//                     break
-//                 case '/':
-//                     result /= number
-//                     break
-//                 case '':
-//                     result = number
-//                     break
-//                 default:
-//                     console.error('error: unknow operator')
-//                     return
-//             }
-//         }
-//         if (i % 2 === 1) {
-//             action = args[i]
-//         }
-//     }
-//     console.log(result)
-// }
-// calc()
